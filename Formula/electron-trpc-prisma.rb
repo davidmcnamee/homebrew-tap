717baaa023fe79cef101433c2add6d6836921cbe
David McNamee <david@mcnamee.io>
2025-08-17 12:11:07 -0400

    Add electron-trpc-prisma formula

---
+++ Formula/electron-trpc-prisma.rb
@@ -0,0 +1,35 @@
+class ElectronTrpcPrisma < Formula
+  desc "Electron app with tRPC and Prisma"
+  homepage "https://github.com/davidmcnamee/electron-trpc-prisma"
+  version "1.0.0"
+  
+  if Hardware::CPU.arm?
+    url "https://github.com/davidmcnamee/electron-trpc-prisma/releases/download/v#{version}/trpc-prisma-electron-#{version}-mac-arm64.tar.gz"
+    sha256 "PLACEHOLDER_SHA256_ARM64"
+  else
+    url "https://github.com/davidmcnamee/electron-trpc-prisma/releases/download/v#{version}/trpc-prisma-electron-#{version}-mac-x64.tar.gz"
+    sha256 "PLACEHOLDER_SHA256_X64"
+  end
+
+  def install
+    app = "trpc-prisma-electron.app"
+    prefix.install app
+    
+    # Create a command-line launcher
+    bin.install_symlink prefix/app/"Contents/MacOS/trpc-prisma-electron" => "trpc-prisma-electron"
+  end
+
+  def caveats
+    <<~EOS
+      To use the app from Applications folder, you may want to copy it:
+        cp -r #{prefix}/trpc-prisma-electron.app /Applications/
+      
+      Or you can run it from the command line:
+        trpc-prisma-electron
+    EOS
+  end
+
+  test do
+    assert_predicate prefix/"trpc-prisma-electron.app", :exist?
+  end
+end
